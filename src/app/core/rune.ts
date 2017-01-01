4d27a41bda092d381b372709431aa5812a59ab49
icepeng <typ0@naver.com>
2017-01-01 17:31:32 +0900

    add some comments to rune class

--- src/app/core/rune.ts
+++ src/app/core/rune.ts
@@ -1,12 +1,12 @@
 export class Rune {
   id: string;
   occupied_id: string;
-  class: number;
-  pri_eff: number[];
-  prefix_eff: number[];
-  sec_eff: number[][];
-  slot_no: number;
-  set_id: number;
-  upgrade_curr: number;
-  rank: number;
+  class: number; // 등급(별)
+  pri_eff: number[]; // 주옵
+  prefix_eff: number[]; // 접두옵
+  sec_eff: number[][]; // 부옵
+  slot_no: number; // 슬롯
+  set_id: number; // 세트
+  upgrade_curr: number; // 업글
+  rank: number; // 색깔
 }
