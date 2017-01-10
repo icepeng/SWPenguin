a3732e86723ef6d20397669f13324746b83b6bbd
icepeng <typ0@naver.com>
2017-01-11 00:54:24 +0900

    made naming tables member of rune service

--- src/app/core/rune.service.ts
+++ src/app/core/rune.service.ts
@@ -4,14 +4,13 @@ import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { MonsterIdName } from './monster-id-name';
 
-const set_id_table = ['X', '활력', '수호', '신속', '칼날', '격노', '집중', '인내', '맹공', 'X', '절망', '흡혈', 'X', '폭주', '응보', '의지', '보호', '반격', '파괴', '투지', '결의', '고양', '명중', '근성'];
-const eff_table = ['X', '깡체', '체퍼', '깡공', '공퍼', '깡방', '방퍼', 'X', '공속', '치확', '치피', '효저', '효적'];
-
 @Injectable()
 export class RuneService {
+  setTable = ['X', '활력', '수호', '신속', '칼날', '격노', '집중', '인내', '맹공', 'X', '절망', '흡혈', 'X', '폭주', '응보', '의지', '보호', '반격', '파괴', '투지', '결의', '고양', '명중', '근성'];
+  effTable = ['X', '깡체', '체퍼', '깡공', '공퍼', '깡방', '방퍼', 'X', '공속', '치확', '치피', '효저', '효적'];
+
   runes: Observable<Rune[]>;
   _runes: BehaviorSubject<Rune[]>;
-  parsedRunes: any;
   dataStore: {
     runes: Rune[];
   }
@@ -20,12 +19,7 @@ export class RuneService {
     this.dataStore = { runes: [] };
     this._runes = <BehaviorSubject<Rune[]>>new BehaviorSubject([]);
     this.runes = this._runes.asObservable();
-    this.runes.subscribe(runes => {
-      this.parsedRunes = [];
-      for (let rune of runes) {
-        this.parsedRunes.push(parseRune(rune));
-      }
-    });
+
   }
 
   import(data): void {
@@ -33,6 +27,34 @@ export class RuneService {
       this.dataStore.runes = list;
       this._runes.next(Object.assign({}, this.dataStore).runes);
     })
+  }
+
+  value(rune, eff) {
+
+  }
+
+  maxValue(rune, eff) {
+
+  }
+
+  expectedValue(rune, eff) {
+
+  }
+
+  parseRune(rune: Rune) {
+    return {
+      occupied_name: MonsterIdName[`n${rune.occupied_master_id}`],
+      set_id: this.setTable[rune.set_id],
+      class: rune.class,
+      slot_no: rune.slot_no,
+      upgrade_curr: rune.upgrade_curr,
+      pri_eff: `${this.effTable[rune.pri_eff[0]]} + ${rune.pri_eff[1]}`,
+      prefix_eff: rune.prefix_eff[0] ? `${this.effTable[rune.prefix_eff[0]]} + ${rune.prefix_eff[1]}` : '',
+      sec_eff_1: rune.sec_eff[0] ? `${this.effTable[rune.sec_eff[0][0]]} + ${rune.sec_eff[0][1]}` : '',
+      sec_eff_2: rune.sec_eff[1] ? `${this.effTable[rune.sec_eff[1][0]]} + ${rune.sec_eff[1][1]}` : '',
+      sec_eff_3: rune.sec_eff[2] ? `${this.effTable[rune.sec_eff[2][0]]} + ${rune.sec_eff[2][1]}` : '',
+      sec_eff_4: rune.sec_eff[3] ? `${this.effTable[rune.sec_eff[3][0]]} + ${rune.sec_eff[3][1]}` : ''
+    };
   }
 
 }
@@ -50,22 +72,6 @@ function pushRune(data): Promise<Rune[]> {
     }
     resolve(list);
   });
-}
-
-function parseRune(rune) {
-  return {
-    occupied_name: MonsterIdName[`n${rune.occupied_master_id}`],
-    set_id: set_id_table[rune.set_id],
-    class: rune.class,
-    slot_no: rune.slot_no,
-    upgrade_curr: rune.upgrade_curr,
-    pri_eff: `${eff_table[rune.pri_eff[0]]} + ${rune.pri_eff[1]}`,
-    prefix_eff: rune.prefix_eff[0] ? `${eff_table[rune.prefix_eff[0]]} + ${rune.prefix_eff[1]}` : '',
-    sec_eff_1: rune.sec_eff[0] ? `${eff_table[rune.sec_eff[0][0]]} + ${rune.sec_eff[0][1]}` : '',
-    sec_eff_2: rune.sec_eff[1] ? `${eff_table[rune.sec_eff[1][0]]} + ${rune.sec_eff[1][1]}` : '',
-    sec_eff_3: rune.sec_eff[2] ? `${eff_table[rune.sec_eff[2][0]]} + ${rune.sec_eff[2][1]}` : '',
-    sec_eff_4: rune.sec_eff[3] ? `${eff_table[rune.sec_eff[3][0]]} + ${rune.sec_eff[3][1]}` : ''
-  };
 }
 
 function assignRune(rune, id) {
