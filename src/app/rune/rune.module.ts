47e6d6f78a901a853e262b57f639ace14b0c9735
icepeng <typ0@naver.com>
2017-01-11 00:58:29 +0900

    update module

--- src/app/rune/rune.module.ts
+++ src/app/rune/rune.module.ts
@@ -5,9 +5,6 @@ import { MaterialModule } from '@angular/material';
 
 import { RuneFilterComponent } from './rune-filter/rune-filter.component';
 import { RuneListComponent } from './rune-list/rune-list.component';
-import { RuneComponent } from './rune.component';
-
-import { RuneListService } from './rune-list.service';
 
 @NgModule({
   imports: [
@@ -18,14 +15,10 @@ import { RuneListService } from './rune-list.service';
   declarations: [
     RuneFilterComponent,
     RuneListComponent,
-    RuneComponent,
   ],
   exports: [
     RuneFilterComponent,
     RuneListComponent
-  ],
-  providers: [
-    RuneListService
   ]
 })
 export class RuneModule { }
